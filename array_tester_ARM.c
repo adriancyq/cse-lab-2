6d813e2ccc65036bc7e63ce6534d4bcef7578a06
Jonathan Chiu <jonathanchiu@jonathans-mbp.attlocal.net>
2016-11-05 18:14:11 -0700

    test code for insert_ARM

--- array_tester_ARM.c
+++ array_tester_ARM.c
@@ -16,21 +16,22 @@ int main(void)
 
     assert(testList->sortedList[0] == 1);
     assert(testList->sortedList[1] == 2);
+    printf("Passed test cases for Insert_ARM.\n");
+    
+    // /* BEGIN REMOVE_VAL: testing deletion of multiple elements */
+    // insert(testList, 6);
 
-    /* BEGIN REMOVE_VAL: testing deletion of multiple elements */
-    insert(testList, 6);
+    // /* Should delete two instances of 6 */
+    // remove_val_ARM(testList, 6);
+    // remove_val_ARM(testList, 4);
+    // remove_val_ARM(testList, 5);
 
-    /* Should delete two instances of 6 */
-    remove_val_ARM(testList, 6);
-    remove_val_ARM(testList, 4);
-    remove_val_ARM(testList, 5);
-
-    /* List is 1, 2, 3 */
-    assert(testList->size == afterRemoveVal);
-    for (int index = 0; index < testList->size; index++) {
-        assert(testList->sortedList[index] == index + 1);
-    }
+    // /* List is 1, 2, 3 */
+    // assert(testList->size == afterRemoveVal);
+    // for (int index = 0; index < testList->size; index++) {
+    //     assert(testList->sortedList[index] == index + 1);
+    // }
 
     /* END REMOVE_VAL: Finished tests for remove_val */
-    printf("Passed test cases for remove_val_ARM.\n");
+    //printf("Passed test cases for remove_val_ARM.\n");
 }
