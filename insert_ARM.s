b9aed5234efd6b4b3c55276eafa4f7c0766e6dbf
Jonathan Chiu <jonathanchiu@jonathans-mbp.attlocal.net>
2016-11-05 18:12:41 -0700

    realloc for insert v2

--- insert_ARM.s
+++ insert_ARM.s
@@ -11,58 +11,110 @@ insert_ARM:
     @ Save caller's registers on the stack
     push {r4-r11, ip, lr}
 
-    @ list *ls is in r0, int val in r1
+    @ Parameters:
+    @ R0: list *ls
+    @ R1: int val
+    @
+    @ Dictionary of registers:
+    @ R4 address of sortedList 
+    @ R5 size 
+    @ R6 capacity
+    @ R7 current index 
+    @ R8 index before current 
+    @ R9 stored list *ls 
+    @ R10 element at current index 
+    @ R11 element at index before current 
+
+
     @-----------------------
 
-    @ TODO If list struct address is 0, return -1
+    @ Null check for list struct pointer, return -1
+    CMP R0 #0
+    SUBEQ R0, R0, #1
+    BEQ end
 
-    @ Get the size (R4), capacity (R5) and sortedlist address (R6)
-    LDR R4, [R0, #4]
-    LDR R5, [R0, #8]
-    LDR R6, [R0]
 
-    @ Insert the value into the next available position
-    STR R1, [R6, R4, LSL #2]
 
-    @ Update the size of the list
-    ADD R4, R4, #1
-    STR R4, [R0, #4]
+    @ Store the address of our list struct in another register 
+    MOV R9, R0 
 
-    @ Get the index of the last element in the list 
-    SUB R4, R4, #1
+    @ Grab sortedList address, size, and capacity 
+    LDR R4, [R9]
+    LDR R5, [R9, #4]
+    LDR R6, [R9, #8]
 
-forloop: 
+    @ Current size is current index 
+    MOV R7, R5 
+
+
+
+    @ Check if the list is full
+    CMP R5, R6
+    BNE listNotEmpty
+
+    @ List is full, so we need to double space and convert to bytes
+    LSL R5, R5, #1
+    LSL R5, R5, #2
+
+    @ Fill parameters of realloc, address of memory location and size
+    MOV R0, R5 
+    MOV R1, R5 
+    bl realloc 
+
+    @ Check that reallocation was successful
+    CMP R0, #0
+    SUBEQ R0, R0, #1
+    BEQ end 
+
+    @ Update address of sortedList in register
+    MOV R4, R0 
+
+    @ Update new list properties: sortedList address and size
+    STR R4, [R9]
+    STR R5, [R9, #4]
+
+
+
+listNotEmpty:
     
-    @ Compare index (R4) with 1, continue until index is less than 1
-    CMP R4, #1
-    BLT endfor 
+    @ Insert the element into the list 
+    STR R1, [R4, R7, LSL #2]
 
-    @ Grab the element at index (R7), and the one before (R9)
-    LDR R7, [R6, R4, LSL #2]
-    SUB R8, R4, #1
-    LDR R9, [R6, R8, LSL #2]
+    @ Increment the list size 
+    ADD R5, R5, #1 
+    STR R5, [R9, #4]
 
-    @ Compare the element at index (R7), and the one before (R9)
-    CMP R7, R9 
 
-    @ If the current one is less than the one before, switch them
-    BGE endif 
 
-    @ Swap one before with current 
-    STR R9, [R6, R4, LSL #2]
-    STR R7, [R6, R8, LSL #2]
+sortLoop: 
+    
+    @ Iterate through list backward, stop at second element 
+    CMP R7, #1
+    MOVLT R0, R7 
+    BLT end 
 
-    @ Update index
-    SUB R4, R4, #1
+    @ Grab the current element and the one before it 
+    LDR R10, [R4, R7, LSL #2]
+    SUB R8, R7, #1
+    LDR R11, [R4, R8, LSL #2]
 
-    @ Restart the loop
-    B forloop
+    @ Compare the element at current index to the one before 
+    CMP R10, R11 
+    BGE end 
 
-endfor:
-endif:  
+    @ Switch the two elements
+    STR R10, [R4, R8, LSL #2]
+    STR R11, [R4, R7, LSL #2]
+
+    @ Update the index and go back to beginning 
+    SUB R7, R7, #1 
+    B sortLoop 
+
+
+end:
 
     @ put your return value in r0 here:
-    MOV R0, R4
+
     @-----------------------
 
     @ restore caller's registers
